412979277bfc48fb4f181ef1694d2e337673aef6
Michael L Heuer <heuermh@acm.org>
2025-04-17 12:11:49 -0500

    update duckdb-parquet-tools to version 1.2

--- Formula/duckdb-parquet-tools.rb
+++ Formula/duckdb-parquet-tools.rb
@@ -1,8 +1,8 @@
 class DuckdbParquetTools < Formula
   desc "Apache Parquet format tools for DuckDB"
   homepage "https://github.com/heuermh/duckdb-parquet-tools"
-  url "https://search.maven.org/remotecontent?filepath=com/github/heuermh/duckdb/duckdb-parquet-tools/1.1/duckdb-parquet-tools-1.1-bin.tar.gz"
-  sha256 "563427a7c216096bc1898f541709f91806b8be548901e03d3e13609318c73513"
+  url "https://search.maven.org/remotecontent?filepath=com/github/heuermh/duckdb/duckdb-parquet-tools/1.2/duckdb-parquet-tools-1.2-bin.tar.gz"
+  sha256 "23c0a7ca60c914cc90d148a9603e057c21cb549186b079e443a6b3b3b263abc1"
   license "Apache-2.0"
 
   depends_on "openjdk"
